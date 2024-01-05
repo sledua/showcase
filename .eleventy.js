ec76f5dba3f3a8e695bd72856378d6a971115a3d
Artem <art.sled.ua@gmail.com>
2024-01-05 14:18:24 +0200

    clean any prop

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/admin");
     eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_SHORT);
     });
     return {
       dir: {
